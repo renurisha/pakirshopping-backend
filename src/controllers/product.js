cb43161261f3c4b6959afead9827441a6334af2f
Renu kumari <renuk1485@gmail.com>
2022-07-12 16:01:14 +0530

    add

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -20,6 +20,28 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+router.get("/product/sorting", async (req, res) => {
+  const page = parseInt(req.query.page) - 1 || 0;
+  const limit = parseInt(req.query.limit) || 5;
+  const search = req.query.search || "";
+  let sort = req.query.sort || "price";
+  req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
+  let sortBy = {};
+  if (sort[1]) {
+    sortBy[sort[0]] = sort[1];
+  } else {
+    sortBy[sort[0]] = "asc";
+  }
+  //const cat = await category.find();
+  const product = await Product.find()
+    .sort(sortBy)
+    .skip(page * limit)
+    .limit(limit);
+  res
+    .status(201)
+    .send({ page: page + 1, limit: limit, sortBy, product: product });
+});
+
 router.get("/product", async (req, res) => {
   const product = await Product.find();
   if (!product) {
@@ -30,6 +52,30 @@ router.get("/product", async (req, res) => {
       product: product,
     });
   }
+});
+
+router.delete("/product/:id", async (req, res) => {
+  Product.findByIdAndDelete(req.params.id).then(() => {
+    return res.send({ message: "product deleted successfully" }).catch(() => {
+      return res.send({
+        message: "something went wrong....",
+      });
+    });
+  });
+});
+router.get("/product/details/:id", async (req, res) => {
+  const product = await Product.findById({ _id: req.params.id });
+  if (!product) {
+    return res.send("this product is not founded");
+  } else {
+    return res.status(201).send({
+      message: "successfully",
+      product: product,
+    });
+  }
+  return res.status(401).send({
+    message: "product Not found..",
+  });
 });
 
 router.post(
@@ -45,8 +91,9 @@ router.post(
 
       category,
       quantity,
-      reviews,
+      review,
       category_name,
+      rating,
 
       createdBy,
     } = req.body;
@@ -64,7 +111,8 @@ router.post(
       description,
       quantity,
       price,
-      reviews,
+      rating,
+      review,
       pictures: files,
       createdBy: req.user._id,
     });
@@ -75,9 +123,67 @@ router.post(
 );
 
 router.get("/data/alldata", async (req, res) => {
-  const cat = await category.find();
-  const product = await Product.find();
-  res.status(201).send({ category: cat, product: product });
+  const search = req.query.search;
+  if (search) {
+    const cat = await category.find();
+    const product = await Product.find({
+      name: { $regex: search, $options: "i" },
+    });
+    res.status(201).send({ category: cat, product: product });
+  } else {
+    const cat = await category.find();
+    const product = await Product.find();
+    res.status(201).send({ category: cat, product: product });
+  }
+});
+router.get(`/product/:slug`, async (req, res) => {
+  const { slug } = req.params;
+  const page = parseInt(req.query.page) - 1 || 0;
+  const limit = parseInt(req.query.limit) || 50;
+  const search = req.query.search || "";
+  //let sort = req.query.sort || "price";
+  let sort;
+  console.log("sortby....", req.query.sort, req.query.orderBy);
+  // req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
+
+  let sortBy = {};
+  if (req.query.sort) {
+    sort = req.query.sort;
+    if (req.query.orderBy) {
+      sortBy[sort] = req.query.orderBy;
+    } else {
+      sortBy[sort] = "asc";
+    }
+  } else {
+    sortBy = {};
+  }
+  // let sortBy = {};
+  // if (sort[1]) {
+  //   sortBy[sort[0]] = sort[1];
+  // } else {
+  //   sortBy[sort[0]] = "asc";
+  // }
+  const cat = await category.findOne({ slug: slug });
+  if (!cat) {
+    return res.status(401).send({ message: "invalid category of products" });
+  } else {
+    const product = await Product.find({ category: cat._id })
+      .sort(sortBy)
+      .skip(page * limit)
+      .limit(limit);
+    return res.status(201).send({
+      product: product,
+      productByPrice: {
+        under10k: product.filter((p) => p.price <= 10000),
+        under20k: product.filter((p) => p.price > 10000 && p.price <= 20000),
+        under30k: product.filter((p) => p.price > 20000 && p.price <= 30000),
+        above30k: product.filter((p) => p.price > 30000),
+      },
+    });
+  }
 });
 
+router.get(`/product/getimage/:path`, (req, res) => {
+  res.download("./src/uploads/" + req.params.path);
+});
 module.exports = router;
