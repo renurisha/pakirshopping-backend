4d0d8bfd4ea842537051bf614d54c1183629b9fa
Renu kumari <renuk1485@gmail.com>
2022-07-13 17:44:58 +0530

    add

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -63,7 +63,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(401).send({ errors: errors.array() });
+      return res.status(202).send({ message: errors.array() });
     }
     const user = await User.findOne({ email: req.body.email });
     if (user) {
@@ -82,9 +82,9 @@ router.post(
           .status(201)
           .send({ message: "login successfully.", data: user });
       }
-      return res.status(401).send({ message: "invalid user..." });
+      return res.status(202).send({ message: "invalid user..." });
     }
-    return res.status(401).send({ message: "user doesn't exist..." });
+    return res.status(202).send({ message: "user doesn't exist..." });
   }
 );
 module.exports = router;
